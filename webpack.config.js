04b1e59d6df3b4d1b49ef8afd35770aa970c266c
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-01-25 12:01:01 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    static: './dist',
+  },
+};
+
+module.exports = config;
