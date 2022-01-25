edee44d909b8b285d363923490bc3d75aa0cbd2d
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-01-25 16:31:09 +0530

    Add breaking changes to ship object

--- tests/ship.spec.js
+++ tests/ship.spec.js
@@ -2,37 +2,84 @@ import { TestWatcher } from 'jest';
 import shipFactory from '../src/ship-factory.js';
 
 describe('ship object testing', () => {
-  let ship;
+  let verticalShip, horizontalShip;
   beforeEach(() => {
-    ship = shipFactory(5);
+    verticalShip = shipFactory(3, 2, 5, true);
+    horizontalShip = shipFactory(2, 3, 3, false);
   });
 
-  test('check if length is set', () => {
-    expect(ship.length).toBe(5);
+  test('checks length is set', () => {
+    expect(verticalShip.length).toBe(5);
+    expect(horizontalShip.length).toBe(3);
   });
 
-  test('check if ship is sunk at start', () => {
-    expect(ship.isSunk()).toBe(false);
+  test('checks ships are not sunk', () => {
+    expect(verticalShip.isSunk()).toBe(false);
+    expect(horizontalShip.isSunk()).toBe(false);
   });
 
-  test('check all position are not hit', () => {
-    for (let i = 0; i < ship.length; i++) {
-      expect(ship.canHit(i)).toBe(true);
+  test('checks all positions in vertical ship is not hit', () => {
+    for (let i = 0; i < verticalShip.length; i++) {
+      expect(
+        verticalShip.canHit(verticalShip.start_x, verticalShip.start_y + i)
+      ).toBe(true);
     }
   });
 
-  test('check hit works', () => {
-    expect(ship.hit(ship.length - 1)).toBe(true);
-    expect(ship.canHit(ship.length - 1)).toBe(false);
-    for (let i = 0; i < ship.length - 1; i++) {
-      expect(ship.canHit(i)).toBe(true);
+  test('checks all positions in horizontal ship is not hit', () => {
+    for (let i = 0; i < horizontalShip.length; i++) {
+      expect(
+        horizontalShip.canHit(
+          horizontalShip.start_x + i,
+          horizontalShip.start_y
+        )
+      ).toBe(true);
     }
   });
 
-  test('check ship can be sunk', () => {
-    for (let i = 0; i < ship.length; i++) {
-      expect(ship.hit(i)).toBe(true);
+  test('check hit works in vertical ship', () => {
+    expect(
+      verticalShip.canHit(verticalShip.start_x, verticalShip.start_y + 1)
+    ).toBe(true);
+
+    expect(
+      verticalShip.hit(verticalShip.start_x, verticalShip.start_y + 1)
+    ).toBe(true);
+
+    expect(
+      verticalShip.canHit(verticalShip.start_x, verticalShip.start_y + 1)
+    ).toBe(false);
+  });
+
+  test('check hit works in horizontal ship', () => {
+    expect(
+      horizontalShip.canHit(horizontalShip.start_x + 1, horizontalShip.start_y)
+    ).toBe(true);
+
+    expect(
+      horizontalShip.hit(horizontalShip.start_x + 1, horizontalShip.start_y)
+    ).toBe(true);
+
+    expect(
+      horizontalShip.canHit(horizontalShip.start_x + 1, horizontalShip.start_y)
+    ).toBe(false);
+  });
+
+  test('check vertical ship can be sunk', () => {
+    for (let i = 0; i < verticalShip.length; i++) {
+      expect(
+        verticalShip.hit(verticalShip.start_x, verticalShip.start_y + i)
+      ).toBe(true);
     }
-    expect(ship.isSunk()).toBe(true);
+    expect(verticalShip.isSunk()).toBe(true);
+  });
+
+  test('check horizontal ship can be sunk', () => {
+    for (let i = 0; i < horizontalShip.length; i++) {
+      expect(
+        horizontalShip.hit(horizontalShip.start_x + i, horizontalShip.start_y)
+      ).toBe(true);
+    }
+    expect(horizontalShip.isSunk()).toBe(true);
   });
 });
