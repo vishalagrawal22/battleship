13f8db52786f1bf906312a86fbda59a0cba99dcd
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-01-25 16:31:43 +0530

    Implement ship with breaking changes

--- src/ship-factory.js
+++ src/ship-factory.js
@@ -1,10 +1,19 @@
-function shipFactory(length) {
+function shipFactory(start_x, start_y, length, isVertical) {
   const isDestroyed = new Array(length);
   for (let i = 0; i < length; i++) {
     isDestroyed[i] = false;
   }
 
-  let canHit = (position) => {
+  let getPosition = (target_x, target_y) => {
+    if (isVertical) {
+      return target_y - start_y;
+    } else {
+      return target_x - start_x;
+    }
+  };
+
+  let canHit = (target_x, target_y) => {
+    const position = getPosition(target_x, target_y);
     if (0 <= position && position < length && !isDestroyed[position]) {
       return true;
     } else {
@@ -12,8 +21,9 @@ function shipFactory(length) {
     }
   };
 
-  let hit = (position) => {
-    if (!canHit(position)) {
+  let hit = (target_x, target_y) => {
+    const position = getPosition(target_x, target_y);
+    if (!canHit(target_x, target_y)) {
       return false;
     }
     isDestroyed[position] = true;
@@ -30,7 +40,7 @@ function shipFactory(length) {
     }, true);
   };
 
-  return { length, hit, canHit, isSunk };
+  return { start_x, start_y, length, isVertical, hit, canHit, isSunk };
 }
 
 export default shipFactory;
