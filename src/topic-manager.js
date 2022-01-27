1c84a842bfc414b44d2f568d1d5ad6e5607ce5a1
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-01-27 08:56:24 +0530

    Add pubsub library

---
+++ src/topic-manager.js
@@ -0,0 +1,11 @@
+import { subscribe as sub, publish as pub } from 'pubsub-js';
+
+function subscribe(topic, callback) {
+  return sub(topic, callback);
+}
+
+function publish(topic, data) {
+  return pub(topic, data);
+}
+
+export { subscribe, publish };
