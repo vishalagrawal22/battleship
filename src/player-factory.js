d782533eee08c6e3f77a48be1003ae582350d096
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-01-29 15:51:38 +0530

    Refactor computerFactory

--- src/player-factory.js
+++ src/player-factory.js
@@ -8,16 +8,118 @@ function playerFactory(num_of_rows, num_of_columns, name) {
 
 function computerFactory(num_of_rows, num_of_columns, name) {
   let { gameboard } = playerFactory(num_of_rows, num_of_columns, name);
+  let searchMoves = [];
+  let destroyMove = null;
   let moves = [];
-  let pendingMoves = [];
-  // oponnentGridInfo can have only three values unknown, hit, miss
-  let opponentGridInfo = new Array(num_of_rows + 1);
   for (let x = 1; x <= num_of_rows; x++) {
-    opponentGridInfo[x] = new Array(num_of_columns + 1);
     for (let y = 1; y <= num_of_columns; y++) {
-      opponentGridInfo[x][y] = 'unknown';
-      moves.push({ x, y });
+      moves.push(moveFactory(x, y));
     }
+  }
+
+  function moveFactory(x, y) {
+    const isValid = () => {
+      if (!(1 <= x && x <= num_of_columns)) {
+        return false;
+      }
+
+      if (!(1 <= y && y <= num_of_rows)) {
+        return false;
+      }
+
+      return true;
+    };
+
+    const isUsed = () => {
+      const ind = moves.findIndex((move) => move.x === x && move.y === y);
+      return ind === -1;
+    };
+
+    const check = () => {
+      if (!isValid()) {
+        return false;
+      }
+
+      if (isUsed()) {
+        return false;
+      }
+
+      return true;
+    };
+
+    const markAsUsed = () => {
+      const ind = moves.findIndex((move) => move.x === x && move.y === y);
+      moves.splice(ind, 1);
+    };
+
+    return { x, y, isValid, isUsed, check, markAsUsed };
+  }
+
+  function randomMoveFactory(x, y) {
+    const type = 'random';
+    const { isValid, isUsed, markAsUsed } = moveFactory(x, y);
+    return { x, y, type, isValid, isUsed, markAsUsed };
+  }
+
+  function searchMoveFactory(x, y, parent_x, parent_y) {
+    const type = 'search';
+    const { isValid, isUsed, markAsUsed, check } = moveFactory(x, y);
+    return {
+      x,
+      y,
+      parent_x,
+      parent_y,
+      type,
+      isValid,
+      isUsed,
+      markAsUsed,
+      check,
+    };
+  }
+
+  function destroyMoveFactory(start_x, start_y, end_x, end_y) {
+    const type = 'destroy';
+    const direction = {
+      x: Math.abs(start_x - end_x),
+      y: Math.abs(start_y - end_y),
+    };
+    const startMove = moveFactory(start_x, start_y);
+    const endMove = moveFactory(end_x, end_y);
+
+    const checkStart = () => {
+      return startMove.check();
+    };
+
+    const checkEnd = () => {
+      return endMove.check();
+    };
+
+    const extend = (move) => {
+      return moveFactory(move.x - direction.x, move.y - direction.y);
+    };
+
+    const checkValidity = () => {
+      return checkStart() && checkEnd();
+    };
+
+    const extendStart = () => {
+      startMove = extend(startMove);
+    };
+
+    const extendEnd = () => {
+      endMove = extend(endMove);
+    };
+
+    return {
+      type,
+      startMove,
+      endMove,
+      checkValidity,
+      checkEnd,
+      checkStart,
+      extendStart,
+      extendEnd,
+    };
   }
 
   let getRandomInt = (lower_bound, upper_bound) => {
@@ -41,30 +143,8 @@ function computerFactory(num_of_rows, num_of_columns, name) {
     }
   }
 
-  function isValid(x, y) {
-    if (!(1 <= x && x <= num_of_columns)) {
-      return false;
-    }
-
-    if (!(1 <= y && y <= num_of_rows)) {
-      return false;
-    }
-
-    return true;
-  }
-
-  function isMoveUsed(x, y) {
-    let ind = moves.findIndex((move) => move.x === x && move.y === y);
-    return ind === -1;
-  }
-
-  function markMoveAsUsed(x, y) {
-    let ind = moves.findIndex((move) => move.x === x && move.y === y);
-    moves.splice(ind, 1);
-  }
-
-  function markAdjacentCellAsPending(x, y) {
-    pendingMoves = [];
+  function addSearchMoves(x, y) {
+    searchMoves = [];
     let diffArray = [
       [1, 0],
       [-1, 0],
@@ -74,18 +154,17 @@ function computerFactory(num_of_rows, num_of_columns, name) {
     diffArray.forEach((diff) => {
       let nx = x + diff[0];
       let ny = y + diff[1];
-      if (isValid(nx, ny) && !isMoveUsed(nx, ny)) {
-        pendingMoves.push({ x: nx, y: ny });
+      const nextSearchMove = searchMoveFactory(nx, ny, x, y);
+      if (nextSearchMove.check()) {
+        searchMoves.push(nextSearchMove);
       }
     });
   }
 
-  function feedback(x, y, wasSuccess) {
+  function feedback(move, wasSuccess) {
+    console.log(move);
     if (wasSuccess) {
-      opponentGridInfo[x][y] = 'hit';
-      markAdjacentCellAsPending(x, y);
-    } else {
-      opponentGridInfo[x][y] = 'miss';
+      addSearchMoves(move.x, move.y);
     }
   }
 
@@ -95,17 +174,18 @@ function computerFactory(num_of_rows, num_of_columns, name) {
     }
 
     let move;
-    if (pendingMoves.length !== 0) {
-      move = pendingMoves.pop();
+    if (searchMoves.length !== 0) {
+      move = searchMoves.pop();
     } else {
       let ind = getRandomInt(0, moves.length - 1);
-      move = moves[ind];
+      move = randomMoveFactory(moves[ind].x, moves[ind].y);
     }
-    markMoveAsUsed(move.x, move.y);
+    move.markAsUsed();
+
     return {
       x: move.x,
       y: move.y,
-      feedback: feedback.bind(null, move.x, move.y),
+      feedback: feedback.bind(null, move),
     };
   };
 
